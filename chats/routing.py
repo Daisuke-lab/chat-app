a462796e28ff40167243e0e8fb19e2c26056836c
daisuke <kikuchidaisuke.zr@gmail.com>
2020-12-05 23:12:33 +0900

    hopefully fixed error

--- chats/routing.py
+++ chats/routing.py
@@ -2,10 +2,20 @@ from django.urls import re_path, path
 
 from . import consumers
 
+
+
 websocket_urlpatterns = [
     #"/$" implys you have to end path with "/"
     #"\w" implys any number
 
 #ws => wss
-    re_path(r'^wss/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    re_path(r'^wss/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
 ]
+
+# websocket_urlpatterns = [
+#     #"/$" implys you have to end path with "/"
+#     #"\w" implys any number
+
+# #ws => wss
+#     re_path(r'^wss/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+# ]
