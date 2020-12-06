b2b4fb2455a9493ff0d307cae81812fbca87f099
daisuke <kikuchidaisuke.zr@gmail.com>
2020-12-06 12:23:42 +0900

    hopefully fixed error

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,8 @@
 
 import os
-from whitenoise import WhiteNoise
 
-from my_project import MyWSGIApp
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-
-application = MyWSGIApp()
-application = WhiteNoise(application, root='static/images')
-
-
-
-# application = get_wsgi_application()
+application = get_wsgi_application()
