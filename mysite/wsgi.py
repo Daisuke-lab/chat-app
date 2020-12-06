64d9510b79fbd46fcb6327cd8364e9bdaa13eb09
daisuke <kikuchidaisuke.zr@gmail.com>
2020-12-06 12:17:31 +0900

    hopefully fixed error

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
 
 import os
+from whitenoise import WhiteNoise
 
+from my_project import MyWSGIApp
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = get_wsgi_application()
+
+application = MyWSGIApp()
+application = WhiteNoise(application, root='static/images')
+
+
+
+# application = get_wsgi_application()
