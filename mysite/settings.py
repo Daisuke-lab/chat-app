da6bcd68643f38c76986029d0b06570efab3ef7d
daisuke <kikuchidaisuke.zr@gmail.com>
2020-12-13 20:02:46 +0900

    hopefully fixed error

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'jeorgia.zr@gmail.com'
+EMAIL_HOST_USER = 'speakup.team.zr@gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_PASSWORD = 'oentswepntjbfmwn'
 EMAIL_USE_TLS = True
