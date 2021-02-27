550916de302dbb9175679dee42751ce8973ca90a
daisuke <daisuke.zr@icloud.com>
2021-02-27 16:18:13 +0900

    gitignore updated3

--- Procfile
+++
@@ -1,3 +0,0 @@
-
-web: daphne mysite.asgi:application --port $PORT --bind 0.0.0.0 -v2
-chatworker: python manage.py runworker --settings=mysite.settings -v2
--- Profile/swipe/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from ..models import Swipe 
-class SwipeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Swipe
-        fields = '__all__'
